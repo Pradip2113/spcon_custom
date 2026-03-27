c4287c0b5b754ca994ea8f2eb16ce94c5a700ee3
Pradip Jadhav <21pradipjadhav@gmail.com>
2026-03-27 12:39:47 +0530

    RMC Rate Queue

--- spcon/spcon/doctype/rcm_rate_set/rcm_rate_set.js
+++ spcon/spcon/doctype/rcm_rate_set/rcm_rate_set.js
@@ -1,15 +1,33 @@
+// // Copyright (c) 2026, Sanpra and contributors
+// // For license information, please see license.txt
+
+// frappe.ui.form.on("RCM Rate Set", {
+// 	set_rate(frm) {
+//         frappe.call({
+//             method: "set_rcm_rate",
+//             doc: frm.doc,
+//             callback: function(r){
+//                 console.log(r)
+//             }
+//         })
+// 	},
+// });
+ 
+
+
 // Copyright (c) 2026, Sanpra and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on("RCM Rate Set", {
 	set_rate(frm) {
-        frappe.call({
-            method: "set_rcm_rate",
-            doc: frm.doc,
-            callback: function(r){
-                console.log(r)
-            }
-        })
+		frappe.call({
+			method: "set_rcm_rate",
+			doc: frm.doc,
+			freeze: true,
+			freeze_message: "Processing in background...",
+			callback: function(r) {
+				frappe.msgprint("RCM Rate update started in background");
+			}
+		});
 	},
 });
- 
--- spcon/spcon/doctype/rcm_rate_set/rcm_rate_set.py
+++ spcon/spcon/doctype/rcm_rate_set/rcm_rate_set.py
@@ -1,3 +1,24 @@
+# # Copyright (c) 2026, Sanpra and contributors
+# # For license information, please see license.txt
+
+# import frappe
+# from frappe.model.document import Document
+
+
+# class RCMRateSet(Document):
+# 	@frappe.whitelist()
+# 	def set_rcm_rate(self):
+# 		if not self.items:
+# 			return
+# 		for row in self.items:
+# 			data = frappe.get_all("BOM Item", {"item_code": row.item_code,  "docstatus": ["!=", 2]}, ["name", "item_code", "custom_rmc_cost", "custom_rmc_amount", "qty"])
+# 			for d in data:
+# 				rmc_amount = row.rate * d.qty
+# 				frappe.set_value("BOM Item", d.name, "custom_rmc_cost", row.rate)
+# 				frappe.set_value("BOM Item", d.name, "custom_rmc_amount", rmc_amount)
+# 		frappe.msgprint("Rate is Set")
+
+
 # Copyright (c) 2026, Sanpra and contributors
 # For license information, please see license.txt
 
@@ -6,14 +27,53 @@ from frappe.model.document import Document
 
 
 class RCMRateSet(Document):
+
 	@frappe.whitelist()
 	def set_rcm_rate(self):
-		if not self.items:
-			return
-		for row in self.items:
-			data = frappe.get_all("BOM Item", {"item_code": row.item_code,  "docstatus": ["!=", 2]}, ["name", "item_code", "custom_rmc_cost", "custom_rmc_amount", "qty"])
-			for d in data:
-				rmc_amount = row.rate * d.qty
-				frappe.set_value("BOM Item", d.name, "custom_rmc_cost", row.rate)
-				frappe.set_value("BOM Item", d.name, "custom_rmc_amount", rmc_amount)
-		frappe.msgprint("Rate is Set")
+		# Enqueue background job
+		frappe.enqueue(
+			method="spcon.spcon.doctype.rcm_rate_set.rcm_rate_set.update_rcm_rate",
+			doc=self.as_dict(),
+			queue="long",
+			timeout=600
+		)
+
+		frappe.msgprint("RCM Rate update started in background")
+
+
+# -----------------------------------------
+# BACKGROUND FUNCTION
+# -----------------------------------------
+
+def update_rcm_rate(doc):
+	doc = frappe._dict(doc)
+
+	if not doc.get("items"):
+		return
+
+	for row in doc.get("items"):
+
+		bom_items = frappe.get_all(
+			"BOM Item",
+			filters={
+				"item_code": row.get("item_code"),
+				"docstatus": ["!=", 2]
+			},
+			fields=["name", "qty"]
+		)
+
+		for d in bom_items:
+
+			rmc_amount = row.get("rate") * d.get("qty")
+
+			frappe.db.set_value(
+				"BOM Item",
+				d.get("name"),
+				{
+					"custom_rmc_cost": row.get("rate"),
+					"custom_rmc_amount": rmc_amount
+				}
+			)
+
+	# Commit once at end
+	frappe.db.commit()
