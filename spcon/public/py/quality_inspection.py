91a8a860e4e09fb4309177ed07ef1b2e8347c62c
Pradip Jadhav <21pradipjadhav@gmail.com>
2026-03-28 11:07:39 +0530

    Quality Inspection Change

--- spcon/public/py/quality_inspection.py
+++ spcon/public/py/quality_inspection.py
@@ -1,5 +1,5 @@
 import frappe
 
 def set_parametor_mandetory(doc, Method=None):
-    if not doc.readings:
-        frappe.throw("Add Atleast one parametor in Readings Table")
+    if not doc.readings and doc.reference_type == "Stock Entry":
+        frappe.throw("Please add at least one parameter to the Readings table.")
