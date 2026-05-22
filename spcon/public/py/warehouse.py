dd2dd4b99e6939b0d895df59e63cf30f2a8d3f3f
Vikaschudmunge <vikaschudmunge5901@gmail.com>
2026-05-22 18:21:38 +0530

    work order and stock entry changes

--- spcon/public/py/warehouse.py
+++ spcon/public/py/warehouse.py
@@ -41,3 +41,42 @@ def get_warehouses_for_cost_center(doctype, txt, searchfield, start, page_len, f
 		},
 	)
 
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def warehouse_query(doctype, txt, searchfield, start, page_len, filters):
+	cost_center = filters.get("cost_center") if filters else None
+	if not cost_center:
+		return []
+
+	return frappe.db.sql(
+		"""
+		select distinct w.name
+		from `tabWarehouse` w
+		inner join `tabCost Center Filter Items` wcc
+			on wcc.parent = w.name
+			and wcc.parenttype = 'Warehouse'
+			and wcc.parentfield = 'custom_cost_center'
+		where
+			wcc.cost_center = %(cost_center)s
+			and w.disabled = 0
+			and w.is_group = 0
+			and (
+				w.name like %(txt)s
+				or w.warehouse_name like %(txt)s
+			)
+			{match_cond}
+		order by
+			if(locate(%(_txt)s, w.name), locate(%(_txt)s, w.name), 99999),
+			w.name
+		limit %(start)s, %(page_len)s
+		""".format(match_cond=get_match_cond(doctype)),
+		{
+			"cost_center": cost_center,
+			"txt": "%%%s%%" % txt,
+			"_txt": txt.replace("%", ""),
+			"start": cint(start),
+			"page_len": cint(page_len),
+		},
+	)
