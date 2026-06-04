ea3046fadaada16332f072201000a8e704a5de18
Vikaschudmunge <vikaschudmunge5901@gmail.com>
2026-06-04 11:55:46 +0530

    fix the permission issue in code in warehouse.py

--- spcon/public/py/warehouse.py
+++ spcon/public/py/warehouse.py
@@ -52,24 +52,24 @@ def warehouse_query(doctype, txt, searchfield, start, page_len, filters):
 
 	return frappe.db.sql(
 		"""
-		select distinct w.name
-		from `tabWarehouse` w
-		inner join `tabCost Center Filter Items` wcc
-			on wcc.parent = w.name
-			and wcc.parenttype = 'Warehouse'
-			and wcc.parentfield = 'custom_cost_center'
+		select distinct `tabWarehouse`.name
+		from `tabWarehouse`
+		inner join `tabCost Center Filter Items` cost_center_filter
+			on cost_center_filter.parent = `tabWarehouse`.name
+			and cost_center_filter.parenttype = 'Warehouse'
+			and cost_center_filter.parentfield = 'custom_cost_center'
 		where
-			wcc.cost_center = %(cost_center)s
-			and w.disabled = 0
-			and w.is_group = 0
+			cost_center_filter.cost_center = %(cost_center)s
+			and `tabWarehouse`.disabled = 0
+			and `tabWarehouse`.is_group = 0
 			and (
-				w.name like %(txt)s
-				or w.warehouse_name like %(txt)s
+				`tabWarehouse`.name like %(txt)s
+				or `tabWarehouse`.warehouse_name like %(txt)s
 			)
 			{match_cond}
 		order by
-			if(locate(%(_txt)s, w.name), locate(%(_txt)s, w.name), 99999),
-			w.name
+			if(locate(%(_txt)s, `tabWarehouse`.name), locate(%(_txt)s, `tabWarehouse`.name), 99999),
+			`tabWarehouse`.name
 		limit %(start)s, %(page_len)s
 		""".format(match_cond=get_match_cond(doctype)),
 		{
