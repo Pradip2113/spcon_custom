5877a41b30b88ce4016835559a5b91c0f1f02eb9
Vikaschudmunge <vikaschudmunge5901@gmail.com>
2026-09-01 10:56:02 +0530

    add item wise/customer wise filter in month-wise sale report

--- spcon/spcon/report/month_wise_sale/month_wise_sale.js
+++ spcon/spcon/report/month_wise_sale/month_wise_sale.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Month-Wise Sale"] = {
+	onload: function(report) {
+		set_group_filter_visibility(report);
+	},
 	filters: [
 		{
 			fieldname: "fiscal_year",
@@ -12,10 +15,28 @@ frappe.query_reports["Month-Wise Sale"] = {
 			reqd: 1,
 		},
 		{
+			fieldname: "group_by",
+			label: __("Group By"),
+			fieldtype: "Select",
+			options: "Customer Wise\nItem Wise",
+			default: "Customer Wise",
+			reqd: 1,
+			on_change: function() {
+				set_group_filter_visibility(frappe.query_report);
+				frappe.query_report.refresh();
+			},
+		},
+		{
 			fieldname: "customer",
 			label: __("Customer"),
 			fieldtype: "Link",
 			options: "Customer",
+		},
+		{
+			fieldname: "item",
+			label: __("Item"),
+			fieldtype: "Link",
+			options: "Item",
 		},
 		{
 			fieldname: "view_by",
@@ -27,3 +48,18 @@ frappe.query_reports["Month-Wise Sale"] = {
 		},
 	]
 };
+
+
+function set_group_filter_visibility(report) {
+	let group_by = report.get_filter_value("group_by") || "Customer Wise";
+	let show_customer = group_by === "Customer Wise";
+
+	report.toggle_filter_display("customer", !show_customer);
+	report.toggle_filter_display("item", show_customer);
+
+	if (show_customer) {
+		report.set_filter_value("item", "");
+	} else {
+		report.set_filter_value("customer", "");
+	}
+}
